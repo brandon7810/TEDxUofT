1d2c658706becbb465e8cfbf1af7c99d261da904
Kai <im.kaiyu@gmail.com>
2014-01-26 16:50:10 -0500

    finalized form elements

--- js/main.js
+++ js/main.js
@@ -1,123 +1,179 @@
 $(function ()
-  { 
-  
-    var name, email, phone, occupation, party_ticket, campus, message, additional_info, dietary_res;
-    
-    $("#wizard").steps({
-      headerTag: "h2",
-      bodyTag: "section",
-      transitionEffect: "slideLeft",
+{ 
 
-      
+  var name, email, phone, occupation, party_ticket, campus, message, additional_info, dietary_res;
+
+  $("#wizard").steps({
+    headerTag: "h2",
+    bodyTag: "section",
+    transitionEffect: "slideLeft",
+
+
       //Events happen in different step
       onStepChanging: function (event, currentIndex, priorIndex) { 
-      
+
         //First step
         if(currentIndex == 0){
           name = $('#applyName').val();
           email = $('#applyEmail').val();
           phone = $('#Phone').val();
-          if(name.length == 0 || email.length == 0 || phone.length ==0){
-            alert("Please complete all the blanks.");
+          if(name.length == 0){
+            $('#applyName').css('border-bottom','1px solid #FF2B06');
             return false;
-          }else{
+          } else if (email.length == 0){
+            $('#applyEmail').css('border-bottom','1px solid #FF2B06');
+          } else {
             return true;
           }
           
         //Second step
-        }else if(currentIndex == 1){
-          occupation = $('#Occupation').val();
-          
-          if(occupation.length == 0){
-            alert("Please complete all the options.");
-            return false;
-          }else{
-            return true;
-          }
-          
+      }else if(currentIndex == 1){
+        occupation = $('#Occupation').val();
+
+        if(occupation.length == 0){
+          $('#Occupation').css('border-bottom','1px solid #FF2B06');
+          return false;
+        }else{
+          return true;
+        }
+
         //Third step
-        }else if(currentIndex == 2){
-          message = $('#applyMessage').val();
-          if (message.length <= 30){
-            alert("You message is too short.");
-            return false;
-          }else{
-            return true;
+      }else if(currentIndex == 2){
+        message = $('#applyMessage').val();
+
+        var max_count = 200;
+        var wordCounts = {};
+
+        $("#applyMessage").on('keyup', function() {
+          var words = this.value.match(/\S+/g).length;
+          if (words > max_count) {
+              // Split the string on first 200 words and rejoin on spaces
+              var trimmed = $(this).val().split(/\s+/, max_count).join(" ");
+              // Add a space at the end to keep new typing making new words
+              $(this).val(trimmed + " ");
           }
+            else {
+                $('#count_left').html(max_count-words);
+            }
+        });
+
+        if (message.length == 0){
+          $('#applyMessage').css('border','1px solid #FF2B06');
+          return false;
+        }else{
+          return true;
+        }
         
         //Fourth step
-        }else if(currentIndex == 3){
-          dietary_res = $('#Dietary_res').val();
-          additional_info = $('#additional_info').val();
-          return true;
+      }else if(currentIndex == 3){
+        dietary_res = $('#Dietary_res').val();
+        additional_info = $('#additional_info').val();
+        return true;
         
         //Fifth
-        }else if(currentIndex == 4){
-          if(!afterparty_yes_bool && !afterparty_no_bool){
-            alert("Please choose one option");
-            return false;
-          }else{
-            return true;
-          }
-        }             
-        
-      },      
+      }else if(currentIndex == 4){
+        if(!afterparty_yes_bool && !afterparty_no_bool){
+          return false;
+        }else{
+          return true;
+        }
+      }             
+
+    },      
       //Event happens when finishing all the steps
       onFinished: function (event, currentIndex) {
         alert("Name: " + name + "\nPhone: " + phone + "\nEmail: " + email + "\nOccupation: " + occupation + "\nMessage: " + message +
-        "\nDietary restrictions: " + dietary_res + "\nAddtional info: " + additional_info + "\nCampus: " + campus + "\nAfter Party: " + party_ticket);
+          "\nDietary restrictions: " + dietary_res + "\nAddtional info: " + additional_info + "\nCampus: " + campus + "\nAfter Party: " + party_ticket);
       }
     });
-    
-    var utm_bool = false;
-    var utsc_bool = false;
-    var utsg_bool = false;
-    $("#campus_utm").click(function(event) {
-      campus = "UTM";
-      utm_bool = true;
-      $("#campus_utm").css('color','red');
-      utsc_bool = false;
-      $("#campus_utsg").css('color','black');
-      utsg_bool = false;
-      $("#campus_usc").css('color','black');
-    });
-    $("#campus_utsg").click(function(event) {
-      campus = "UTSG";
-      utm_bool = false;
-      $("#campus_utm").css('color','black');
-      utsc_bool = false;
-      $("#campus_utsc").css('color','black');
-      utsg_bool = true;
-      $("#campus_utsg").css('color','red');
-    });
-    $("#campus_utsc").click(function(event) {
-      campus = "UTSC";
-      utm_bool = false;
-      $("#campus_utm").css('color','black');
-      utsc_bool = true;
-      $("#campus_utsc").css('color','red');
-      utsg_bool = false;
-      $("#campus_utsg").css('color','black');
-    });
-    
-    var afterparty_yes_bool = false;
-    var afterparty_no_bool = false;
-    $("#afterparty_yes").click(function(event) {
-      party_ticket = "YES";
-      afterparty_yes_bool = true;
-      $("#afterparty_yes").css('color','red');
-      afterparty_no_bool = false;
-      $("#afterparty_no").css('color','black');
-    });
-    $("#afterparty_no").click(function(event) {
-      party_ticket = "NO";
-      afterparty_no_bool = true;
-      $("#afterparty_no").css('color','red');
-      afterparty_yes_bool = false;
-      $("#afterparty_yes").css('color','black');
-    });
-    
-  });
+
+var utm_bool = false;
+var utsc_bool = false;
+var utsg_bool = false;
+var other_bool = false;
+$("#campus_utm").click(function(event) {
+  campus = "UTM";
+  utm_bool = true;
+  $("#campus_utm").css('background','#FF2B06');
+  $("#campus_utm").css('color','white');
+  utsc_bool = false;
+  $("#campus_utsg").css('background','white');
+  $("#campus_utsg").css('color','#FF2B06');
+  utsg_bool = false;
+  $("#campus_utsc").css('background','white');
+  $("#campus_utsc").css('color','#FF2B06');
+  other_bool = false;
+  $("#campus_other").css('background','white');
+  $("#campus_other").css('color','#FF2B06');
+});
+$("#campus_utsg").click(function(event) {
+  campus = "UTSG";
+  utm_bool = false;
+  $("#campus_utm").css('background','white');
+  $("#campus_utm").css('color','#FF2B06');
+  utsc_bool = false;
+  $("#campus_utsc").css('background','white');
+  $("#campus_utsc").css('color','#FF2B06');
+  utsg_bool = true;
+  $("#campus_utsg").css('background','#FF2B06');
+  $("#campus_utsg").css('color','white');
+  other_bool = false;
+  $("#campus_other").css('background','white');
+  $("#campus_other").css('color','#FF2B06');
+});
+$("#campus_utsc").click(function(event) {
+  campus = "UTSC";
+  utm_bool = false;
+  $("#campus_utm").css('background','white');
+  $("#campus_utm").css('color','#FF2B06');
+  utsc_bool = true;
+  $("#campus_utsc").css('background','#FF2B06');
+  $("#campus_utsc").css('color','white');
+  utsg_bool = false;
+  $("#campus_utsg").css('background','white');
+  $("#campus_utsg").css('color','#FF2B06');
+  other_bool = false;
+  $("#campus_other").css('background','white');
+  $("#campus_other").css('color','#FF2B06');
+});
+$("#campus_other").click(function(event) {
+  campus = "Other";
+  utm_bool = false;
+  $("#campus_utm").css('background','white');
+  $("#campus_utm").css('color','#FF2B06');
+  utsc_bool = false;
+  $("#campus_utsg").css('background','white');
+  $("#campus_utsg").css('color','#FF2B06');
+  utsg_bool = false;
+  $("#campus_utsc").css('background','white');
+  $("#campus_utsc").css('color','#FF2B06');
+  other_bool = true;
+  $("#campus_other").css('background','#FF2B06');
+  $("#campus_other").css('color','white');
+});
+
+var afterparty_yes_bool = false;
+var afterparty_no_bool = false;
+$("#afterparty_yes").click(function(event) {
+  party_ticket = "YES";
+  afterparty_yes_bool = true;
+  $("#afterparty_yes").css('background','#FF2B06');
+  $("#afterparty_yes").css('color','white');
+  afterparty_no_bool = false;
+  $("#afterparty_no").css('color','#FF2B06');
+  $("#afterparty_no").css('background','white');
+});
+$("#afterparty_no").click(function(event) {
+  party_ticket = "NO";
+  afterparty_no_bool = true;
+  $("#afterparty_no").css('background','#FF2B06');
+  $("#afterparty_no").css('color','white');
+  afterparty_yes_bool = false;
+  $("#afterparty_yes").css('color','#FF2B06');
+  $("#afterparty_yes").css('background','white');
+});
+
+});
 
 $('#ContactSubmit').click(function(){
   var name = $('#Name').val();
@@ -136,7 +192,7 @@ $('#ContactSubmit').click(function(){
     $.post( "contactengine.php", { Name: name, Email: email, Message: message } );
     
     $(':input','#contactform')
-      .not(':button, :submit, :reset, :hidden')
-      .val('');  
+    .not(':button, :submit, :reset, :hidden')
+    .val('');  
   }
 });
